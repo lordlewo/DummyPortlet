d661f38b15b5a3e2903deac849f845a7a7293141
Czentye Bence Levente <czentye.bence.levente@webtown.hu>
2016-04-04 15:44:30 +0200

    Initial commit

---
+++ docroot/WEB-INF/src/hu/levi/dummy/portlet/DummyPortlet.java
@@ -0,0 +1,11 @@
+package hu.levi.dummy.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class DummyPortlet
+ */
+public class DummyPortlet extends MVCPortlet {
+ 
+
+}
